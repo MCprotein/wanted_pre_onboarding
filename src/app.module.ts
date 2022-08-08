c5223be19b075898e841bef364c37482acdcd2c6
chss3339 <chss3339@gmail.com>
2022-08-09 03:33:18 +0900

    chore: db 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UsersModule } from './users/users.module';
+import { CompaniesModule } from './companies/companies.module';
+import { PostingsModule } from './postings/postings.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    CompaniesModule,
+    PostingsModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASENAME,
+      entities: [__dirname + '/**/*.entity{.ts, .js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
