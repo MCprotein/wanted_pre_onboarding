29bd0cf7b1ee41e0fbc5168fa936a45e4aab5fd2
chss3339 <chss3339@gmail.com>
2022-08-11 16:16:47 +0900

    feat: 채용공고 중복 지원 방지 기능 추가

--- src/lists/lists.service.ts
+++ src/lists/lists.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ApplyDto } from './dto/apply.dto';
@@ -12,6 +12,18 @@ export class ListsService {
   ) {}
 
   async createList(applyDto: ApplyDto): Promise<List> {
+    const { user } = applyDto;
+    const exitedList = await this.listRepository
+      .createQueryBuilder('list')
+      .leftJoinAndSelect('list.user', 'user')
+      .where('user.id = :id', { id: user })
+      .getRawOne();
+    if (exitedList) {
+      throw new ForbiddenException(
+        '이미 채용공고에 지원하여 더이상 지원하실 수 없습니다.',
+      );
+    }
+
     const createdList = this.listRepository.create(applyDto);
     await this.listRepository.save(createdList);
     return createdList;
