97168e74aac461639dc3a3fdfc853e070a78c343
chss3339 <chss3339@gmail.com>
2022-08-11 23:20:52 +0900

    fix: Jest 모듈 못찾는 에러 해결

--- src/postings/postings.service.spec.ts
+++ src/postings/postings.service.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Posting } from './postings.entity';
+import { Company } from 'src/companies/companies.entity';
 import { PostingsService } from './postings.service';
 
 describe('PostingsService', () => {
-  let service: PostingsService;
+  let postingsService: PostingsService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [PostingsService],
     }).compile();
 
-    service = module.get<PostingsService>(PostingsService);
+    postingsService = module.get<PostingsService>(PostingsService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('create', async () => {
+    const companyMock: Company = {
+      id: 1,
+      name: '네이버',
+      nation: '한국',
+      location: '분당',
+      postings: [],
+    };
+    console.log(companyMock);
+    const posting = await postingsService.createPosting({
+      companyId: companyMock,
+      position: '카카오톡 주니어 개발자',
+      reward: 1000000,
+      content: '원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..',
+      skill: 'Python',
+    });
+    const expected = new Posting();
+    expected.company = companyMock;
+    expected.position = '카카오톡 주니어 개발자';
+    expected.reward = 1000000;
+    expected.content =
+      '원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..';
+    expected.skill = 'Python';
+    expect(expected).toEqual(posting);
   });
 });
